0cea193eea1d1cc4dee19495fa4300a3677bc580
der1sebi <47915100+der1sebi@users.noreply.github.com>
2020-12-15 21:41:15 +0100

    added sleigh

---
+++ scripts/christmasSleigh.lua
@@ -0,0 +1,56 @@
+--[[---------------------------------------------------------------------------\
+| ||\\    //||       /|¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯¯\ |
+| || \\  // ||  (o_ / |                  SUPPLEMENTARY FILE                  | |
+| ||  \\//  ||  //\/  |                         ----                         | |
+| ||   \/   ||  V_/_  |                   CHRISTMAS SLEIGH                   | |
+| ||        ||        |‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗‗/ |
+\---------------------------------------------------------------------------]]--
+
+local winterWonderland = ...
+
+--[[--------------------------- PREFABS & MATERIALS ---------------------------]]--
+
+winterWonderland:registerAssetId("models/sleigh.fbx/Prefab/christmas_sleigh", "PREFAB_CHRISTMAS_SLEIGH_PART")
+
+--[[winterWonderland:registerAssetId("models/ChristmasTreeMonument.fbx/Materials/Material.Branches", "MATERIAL_BRANCHES")
+
+winterWonderland:override({
+    Id = "MATERIAL_BRANCHES",
+    HasAlphaTest = true
+})]]--
+
+--[[------------------------ BUILDINGS & BUILDING PARTS -----------------------]]--
+
+winterWonderland:register({
+	DataType = "BUILDING",
+	Id = "CHRISTMAS_SLEIGH",
+	Name = "CHRISTMAS_SLEIGH_NAME",
+	Description = "CHRISTMAS_SLEIGH_DESC",
+	BuildingType = "DECORATION",
+	AssetCoreBuildingPart = "CHRISTMAS_SLEIGH_PART"
+})
+
+winterWonderland:register({
+	DataType = "BUILDING_PART",
+	Id = "CHRISTMAS_SLEIGH_PART",
+	Description = "CHRISTMAS_SLEIGH_PART_DESC",
+	ConstructorData = {
+		DataType = "BUILDING_CONSTRUCTOR_DEFAULT",
+		CoreObjectPrefab = "PREFAB_CHRISTMAS_SLEIGH_PART"
+	},
+	BuildingZone = {
+		ZoneEntryList = {
+            {
+			Polygon = polygon.createCircle( 5.5 , { 0, 0 } , 36 ),
+			Type = { DEFAULT = true, NAVIGABLE = false, GRASS_CLEAR = true }
+			}
+        }
+	},
+	ConstructionVisual = nil,
+	Cost = {
+		RessourcesNeeded = {}
+	},
+    IsVisibleWhenBuilt = true
+})
+
+--[[----------------------------- PARTICLE SYSTEMS ----------------------------]]--
